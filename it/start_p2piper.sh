0303c180c0923aed7a44fa30b119138da0668e5b
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-03-25 14:31:01 -0700

    node.js version bump

--- it/start_p2piper.sh
+++ it/start_p2piper.sh
@@ -3,7 +3,7 @@
 apt-get -y update
 apt-get -y install ruby wget curl
 
-curl -sL https://deb.nodesource.com/setup_12.x -o nodesource_setup.sh
+curl -sL https://deb.nodesource.com/setup_15.x -o nodesource_setup.sh
 chmod +x ./nodesource_setup.sh
 apt-get -y install nodejs
 
