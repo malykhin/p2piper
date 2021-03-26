f51f455daa2cc6bcf31c517ba2adb28d512358ab
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-03-26 14:42:38 -0700

    workdir to start

--- app_start.sh
+++ app_start.sh
@@ -4,5 +4,7 @@ sudo cp /home/ubuntu/app/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
-sudo /home/ubuntu/app/node_modules/pm2/bin/pm2 delete all
-sudo /home/ubuntu/app/node_modules/pm2/bin/pm2 start /home/ubuntu/app/ecosystem.config.js
+cd /home/ubuntu/app
+
+sudo ./node_modules/pm2/bin/pm2 delete all
+sudo ./node_modules/pm2/bin/pm2 start ./ecosystem.config.js
