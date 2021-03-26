864eac90afdef3ea8bb9de46997261924e6f8ac6
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-03-25 20:00:21 -0700

    startup fix

--- app_start.sh
+++ app_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cp /home/ubuntu/app/p2piper/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+cp /home/ubuntu/app/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
-sudo /home/ubuntu/app/p2piper/node_modules/pm2/bin/pm2 start /home/ubuntu/app/p2piper/ecosystem.config.js
+sudo /home/ubuntu/app/node_modules/pm2/bin/pm2 start /home/ubuntu/app/ecosystem.config.js
