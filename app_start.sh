01f22d5338780841f34fb00069f8981b1814ca86
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-03-25 20:15:38 -0700

    sudo cp cloudwatch agent

--- app_start.sh
+++ app_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cp /home/ubuntu/app/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo cp /home/ubuntu/app/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
 sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
 
