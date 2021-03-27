d505aff608b2022c8120f7bab2b910c83231b05e
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-03-26 19:30:18 -0700

    no tls for redis

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
         ORIGIN: 'http://p2piper-prod-alb-1781782710.us-east-1.elb.amazonaws.com',
         REDIS_HOST: 'p2piper-prod-cluster-example.gxxc44.0001.use1.cache.amazonaws.com',
         REDIS_PORT: 6379,
-        REDIS_USE_TLS: true,
+        REDIS_USE_TLS: false,
         AWS_REGION: 'us-east-1',
         LOG_GROUP_NAME: 'p2piper',
         LOG_STREAM_NAME: 'p2piper-prod',
