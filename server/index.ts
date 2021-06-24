15739843cc6964edbe6368afde94e4a028840034
Denys Malykhin <denys.malykhin@solidopinion.com>
2021-06-23 21:43:56 -0700

    Checking rooms in redis adapter

--- server/index.ts
+++ server/index.ts
@@ -50,9 +50,9 @@ app.prepare().then(() => {
     logger.info(`connection_${sessionId}_${token}`)
 
     if (token) {
-      const roomsMap = io.of('/').adapter.rooms
+      const rooms = await (io.of('/').adapter as any).allRooms()
 
-      if (!roomsMap.has(token)) {
+      if (!rooms.has(token)) {
         logger.info(`${token} is not in the adapter, redirecting to the root`)
         socket.emit('root_redirect')
       }
